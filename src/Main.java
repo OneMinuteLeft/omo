1d41afd7ae3f193f95456e72a09a2c00aefa3f26
pzaszp <lifeslikethis@keemail.me>
2021-12-25 19:23:36 -0500

    Server list + input checking; goes nowhere yet!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner; // for user input
+
 public class Main {
+  static void servlist() {
+    // set input 'stuff'
+    Scanner servobj = new Scanner(System.in);
+    String servnum;
+
+    // list servers
+    System.out.println("<< Server List >>");
+    System.out.println("1 | OneMinuteLeft serv");
+    System.out.println("Type the corresponding # to connect to a listed server.");
+    servnum = servobj.nextLine();
+
+    // check input
+    if (servnum.equals("1")) {
+      System.out.println("Sending you to OneMinuteLeft...");
+    } else {
+      System.out.println("INPUT ERROR: invalid server number [provided " + servnum + "]");
+    }
+}
+
   public static void main(String[] args) {
-    System.out.println("OneMinuteLeft v0.0.1");
+    System.out.println("One Minute Online v0.1.0");
+    servlist();
+
   }
 }
